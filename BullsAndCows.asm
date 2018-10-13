bc6a0e2c54f4de94294cca3dbbe344a71dc20fe9
BuildTools <jacobsenscott07@gmail.com>
2018-10-12 20:15:01 -0500

    Changed to manually loading 4 bytes in reverse order instead of lw -> flip, added option to give up

--- BullsAndCows.asm
+++ BullsAndCows.asm
@@ -1,8 +1,9 @@
 .data
 	words: .asciiz "HAVEWITHTHISTHEYFROMWHATMAKEKNOWTIMEYEARWHENTHEMSOMETAKEINTOJUSTYOURCOMETHANLIKETHENMOREWANTALSOMOREFINDGIVEMANYONLYVERYBACKLIFEWORKDOWNOVERLASTWHENMOSTMUCHMEANSAMEHELPTALKTURNHANDSHOWPARTOVERSUCHCASEMOSTEACHHEARWORKPLAYMOVELIKELIVEHOLDNEXTMUSTHOMEFACTWORDSIDEKINDFOURHEADLONGBOTHLONGHOURGAMELINELOSECITYMUCHNAMEFIVEONCEREALTEAMBESTIDEABODYLEADBACKONLYFACEREADSURESUCHGROWOPENWALKGIRLBOTHABLELOVEWAITSENDHOMESTAYPLANYEAHCARELATEHARDROLERATEDRUGSHOWWIFEMINDHOPEVIEWTOWNROADTRUEJOINPICKWEARFORMSITEBASESTARHALFEASYCOSTFACELANDNEWSLOVEOPENSTEPTYPEDRAWFILMHAIRTERMRULERISKFIREBANKWESTRESTDEALPASTGOALDROPPLANUPONPUSHNOTEFINENEARPAGETHANRACEEACHRISEEASTSAVETHUSSIZEFUNDSIGNLISTHARDLEFTDEALFAILNAMESORTBLUESONGDARKHANGROCKNOTEHELPCOLDFORMMAINCARDSEATNICEFIRMCAREHUGETALKHEADBASEPAINPLAYWIDEFISHTRIPUNITBESTPASTFEARSIGNHEATSINGWHOMSKINDOWNITEMSTEPYARDBEATTENDTASKSHOTWISHSAFERICHVOTEBORNWINDFASTCOSTLIKEBIRDHURTHOPEVOTETURNONCECAMPDATEVERYHOLESHIPPARKSPOTLACKBOATGAINHIDEGOLDCLUBFARMBANDRIDEWILDEARNTINYPATHSHOPFOLKLIFTJUMPWARMSOFTGIFTPASTWAVEMOVEDENYSUITBLOWKINDBURNSHOEVIEWBONEWINEMEANFIRETOURGRABFAIRPAIRTAPEHIRENEXTLADYNECKLEANHATEMALEARMYSHUTLOTSRAINFUELLEAFLEADSALTSOULBEARTHINHALFOKAYCODEJURYDESKFEARLIKELASTRINGMARKLOANCREWMALEMEALCASHLINKNOSEFILESICKDUTYSLOWZONEWAKEWARNSNOWSLIPMEATSOILLATEGOLFJUSTUSERPARTUSEDBOWLLONGHOSTRELYBACKDEBTTANKBONDFILEWINGMEANPOURSTIRTEARHERORESTBUSYCOPYCITEGRAYDISHCORERUSHRISEVASTLACKFLOWTONEAIDSGATEHANDLANDMILKCASTRIDELIVEPLUSMINDWEAKLISTWRAPMARKDRAGDIETWASHPOSTDARKCHIPSELFBIKESLOWLINKLAKEBENDGAINARABWALKSANDRULELOCKTEARPOSESALEMINETALEJOKECOATURGEDUSTGLADPACKIRONSUREKINGBEANPEAKVARYWIREHOLYRINGTWINSTOPLUCKRACEBURYPRAYPUREBELTFLAGCORNCROPLINEDATEPINKBUCKPOEMBINDMAILTUBEQUITJAILPACECAKEMINEDROPFASTPACKFLATWAGESNAPGEARWAVESPINRANKBEATWINDLOSTLIKEBEARPANTWIPEPORTDIRTRICEFLOWDECKPOLEMODEBAKESINKSWIMTIREHOLDFADESPOTMASKEASYLOADFATEOVENPOETPALELOADLAWNPLOTMATHTAILPALMSOUPPILEFUNDAIDEMYTHMENURATELOUDAUTOBITEPINERISKCHEFSUITSHITCOPEHOSTWISEACIDLUNGFIRMUGLYROPESAKEGAZECLUEDEARCOALSIGHDAREOKAYROSERAILRANKNORMSTEMRAPEHUNTECHOBARERENTSHOPEVILSLAMMELTPARKCOLDFOLDDUCKDOSETRAPLENSLENDNAILCAVEHERBWISHWARMLASTSUCKLEAPPASTPONDDUMPLIMBTUNEHARMHORNBLUEGRIPBEAMRUSHFORKDISKLOCKBLOWEXITSHIPMILDAMIDLOUDHERSBITEEVILORALFISTBATHBOLDTUNEHINTFLIPBIASLAMPCHINARABCHOPSILKRAGEWAKEDAWNTIDESEALSINKTRAPSCANCARTSTEMMATESLAPOURSHEATBARNTUCKDRUMPOSTSAILNESTNEARLANECAGERACKWOLFGRINSEALAUNTROCKRENTCALMHAULRUINBUSHCLIPEXAMSTAREDITWHIPBOILPORKSOCKNEARJUMPSEXYSEATLIONCASTCORDHARMSORTSOAPCUTESHEDICONHEALCOINSTAYDAMNCASEGAZEHIKESACKTRAYCOUPSKIPSOLEJOKEPILECURECUREFAMEATOPTHISGRINRAINCHEWDUMBBULKGOATNEATPARTPOKESOARCALMCLAYFAREDISCSOFAFISHSOAKSLOTRIOTTILEPLEACOPYBOLTDOCKTRIMSPIT"
 	numWords: .word 673
-	inputPrompt: .asciiz "Enter your guess: "
-	inputBuffer: .word 0
+	inputPrompt: .asciiz "Enter your guess (STOP to give up): "
+	inputBuffer: .space 5
+	giveUpOutput: .asciiz "\nYou gave up! The word was: "
 .text
 	lw	$a1, numWords		# Upperbound for random number generation
 	li	$v0, 42			# Syscall for generating random number
@@ -10,10 +11,8 @@
 	sll	$a0, $a0, 2		# Multiply by 4 because each word is 4 bytes long
 	la	$t0, words		# $t0 contains start address of words data segment
 	add	$a0, $t0, $a0		# Add word offset to make $a0 store position of start of word
-	lw	$a0, ($a0)		# Load the reverse word into $a0
-	jal 	flipString		# Flip contents of $a0
-	move	$s0, $v0		# Store flipped string into $s0
-	
+	jal	loadFourBytes		# Load the word starting at $a0 (this is necessary instead of lw because the word may not start at a word boundary)
+	move	$s0, $v0		# Store string into $s0
 					# At this point, the only register that needs to stay the same is $s0, which stores the word that was retrieved from the list of words
 	li	$v0, 30			# Get system time
 	syscall				# Start timer
@@ -25,19 +24,45 @@
 	la 	$a0, inputBuffer	# $a0 will store the address that the input is stored at
 	li 	$a1, 5			# Read 4 characters (1 for null terminator)
 	syscall				# Read the string, $a0 stores address of input		
-	lw	$a0, ($a0)		# Store input into $a0
-	jal 	flipString		# Flip contents of $a0
-	move	$s1, $v0		# Store flipped string into $s1
-	li	$v0, 10
-	syscall				# Exit (only here temporarily)
 	
-flipString:				# $a0 should contain string to be flipped. $v0 will contain flipped string
-	li	$t0, 0xFF		# Mask for extracting characters
-	li	$v0, 0			# Start out with empty register
-moveChar:
-	sll	$v0, $v0, 8		# Move on to next digit
-	and	$t1, $a0, $t0		# Get first (which is really last) character out of $a0 and store into $t1
-	or	$v0, $v0, $t1		# Write digit into $v0
-	srl	$a0, $a0, 8		# Move on to next digit
-	bne	$a0, $zero, moveChar 	# Repeat process for every digit
+	jal	loadFourBytes		# Load the word starting at $a0 (this is necessary instead of lw because the word may not start at a word boundary)
+	move	$s1, $v0		# Store string into $s1
+
+	li	$t0, 0x53544F50		# $t0 will contain "STOP"
+	beq	$t0, $s1, giveUp
+	li	$v0, 10
+	syscall
+	
+printWord:				# $a0 should contain string to be printed
+	li	$t0, 0xFF000000		# Mask for getting characters
+	li	$t1, 24			# Number of bits to shift to (to get first byte)
+	move	$t3, $a0		# Store a copy of $a0 in $t3
+printChar:
+	and	$a0, $t3, $t0		# Extract char into $a0
+	srlv	$a0, $a0, $t1		# Move char into first byte
+	srl	$t0, $t0, 8		# Move on to next char
+	subi	$t1, $t1, 8		# Move on to next char
+	li	$v0, 11			# Syscall for printing char
+	syscall				# Print extracted char
+	bne	$t0, $zero, printChar	# Repeat for every char
 	jr	$ra
+
+					# This function takes an address, and adds 4 bytes starting at that address to $v0, in reverse order
+loadFourBytes:				# $a0 should contain the base address, $v0 will contain the word that was loaded.
+	li	$t0, 4			# $t0 will be the counter to make sure this only runs 4 times
+	li	$v0, 0			# Reset $v0
+loadByte:
+	sll	$v0, $v0, 8		# Move on to next byte
+	lbu	$t1, ($a0)		# Store byte into $t1
+	or	$v0, $v0, $t1		# Write byte into $v0
+	addi	$a0, $a0, 1		# Move on to next byte
+	subi	$t0, $t0, 1		# Keep track of how many bytes are left
+	bne	$t0, $zero, loadByte	# Load all 4 bytes
+	jr	$ra
+	
+giveUp:
+	la	$a0, giveUpOutput	# Print giveUpOutput
+	li	$v0, 4			# Print string syscall
+	syscall				# Print string
+	move	$a0, $s0		# Get $s0 ready for printing
+	jal	printWord		# Print $s0
